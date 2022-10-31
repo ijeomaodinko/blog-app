6d3d4ce5aab94479dfb0b745bbc94eba8c3374b2
ijeomaodinko <ijeomaodinko@gmail.com>
2022-10-31 19:42:52 -0400

    Add files for user and post

---
+++ app/controllers/post_controller.rb
@@ -0,0 +1,9 @@
+class PostController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'post/index'
+  get 'post/show'
+  get 'users/index'
+  get 'users/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
