d71fc925853531d5c8abfab3408af4bea40bbc75
ijeomaodinko <ijeomaodinko@gmail.com>
2022-11-03 12:05:35 -0400

    Add update on files

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,11 @@
 class PostsController < ApplicationController
   def index
+    @user = User.find(params[:user_id])
     @posts = Post.all
   end
 
   def show
     @post = Post.find(params[:id])
+    @comments = @post.comments
   end
 end
