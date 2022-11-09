db6cc3e2b42dbed493a79fb4b0fce5ea5361c703
ijeomaodinko <ijeomaodinko@gmail.com>
2022-11-09 12:44:58 -0500

    Add update to files

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users 
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
   get 'likes/likes'
   get 'comments/comments'
   get 'posts/index'
