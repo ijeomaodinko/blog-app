db10f518a5f91ece48fe78ea51fcfc62a3ceb6b8
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-11-09 20:55:45 +0300

    Fix spec errors

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -1,27 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe 'users/show.html.erb', type: :feature do
-    before(:each) do
-      @user1 = User.create(name: 'Moon', photo: 'https://images.pexels.com/photos/1805164/pexels-photo-1805164.jpeg?auto=compress&cs=tinysrgb&w=400',
-       bio: 'FullStack Developer from Porland.', posts_counter: 2)
-     
-      @post1 = Post.create(
-        author_id: @user1.id, 
-        title: 'Greating', 
-        text: 'Hello everyone is awesome day for all', 
-        comments_counter: 1, 
-        likes_counter: 0
-      )
+  before(:each) do
+    @user1 = User.create(name: 'Moon', photo: 'https://images.pexels.com/photos/1805164/pexels-photo-1805164.jpeg?auto=compress&cs=tinysrgb&w=400',
+                         bio: 'FullStack Developer from Porland.', posts_counter: 2)
 
-      (0..1).each do |id|
+    @post1 = Post.create(
+      author_id: @user1.id,
+      title: 'Greating',
+      text: 'Hello everyone is awesome day for all',
+      comments_counter: 1,
+      likes_counter: 0
+    )
+
+    (0..1).each do |_id|
       @comment = Comment.create(
-        post_id: @post1.id, 
-        author_id: @user1.id, 
-        text: 'Hi, Excellence' )
-      end
+        post_id: @post1.id,
+        author_id: @user1.id,
+        text: 'Hi, Excellence'
+      )
+    end
 
-      visit  user_posts_path(@user1)
-end
+    visit user_posts_path(@user1)
+  end
 
   it 'should show post title' do
     expect(page).to have_content('Greating')
