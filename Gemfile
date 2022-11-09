3ca8109e42e039c597341802779fea4cdb895cb6
ijeomaodinko <ijeomaodinko@gmail.com>
2022-11-09 11:53:02 -0500

    Add device setup

--- Gemfile
+++ Gemfile
@@ -47,9 +47,12 @@ gem 'rubocop', '>= 1.0', '< 2.0'
 gem 'rails-controller-testing'
 # Use Sass to process CSS
 # gem "sassc-rails"
-gem 'capybara'
+
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
+
+gem 'devise', '~> 4.8', '>= 4.8.1'
+
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :posts, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
 
-  # validates :name, presence: true
-  # validates :posts_counter, numericality: { only_integar: true, greater_than_0r_qual_to: 0 }
+  validates :name, presence: true
+  validates :posts_counter, numericality: { only_integar: true, greater_than_0r_qual_to: 0 }
 
   def most_recent_post
     posts.order(created_at: :DESC).limit(3)
